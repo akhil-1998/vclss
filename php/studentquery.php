4913f991a516166e92bd414f6adbe25d3b99baa2
Akhil M Anil <70442264+akhil-1998@users.noreply.github.com>
2021-08-31 11:09:41 +0530

    Add files via upload

---
+++ php/studentquery.php
@@ -0,0 +1,28 @@
+<?php
+    // PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vclassserver.database.windows.net,1433; Database = vclassdb", "akhilmanil", "akhil@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "akhilmanil", "pwd" => "akhil@123", "Database" => "vclassdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:vclassserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    $tsql= "SELECT * FROM users";
+    $getResults= sqlsrv_query($conn, $tsql);
+   
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['firstname'] . " " . $row['lastname'] . PHP_EOL);
+     echo('<br>');
+    }
+    sqlsrv_free_stmt($getResults);
+?> 
+
